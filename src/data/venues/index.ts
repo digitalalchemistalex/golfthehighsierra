1b480b85db05e8bf9ef67aaa909856ed3e3ed0aa
Digital Alchemist Alex <alex@digitalalchemist.com>
2026-02-17 05:41:34 +0000

    feat: add 26 Eldorado sub-pages (15 rooms, 9 restaurants, 2 bars)
    - 9 restaurants: Roxy, La Strada, Brew Brothers, Millies, Ichiban, Sushi Sake, Hidden Pizza, Coffee Company, Starbucks
    - 2 bars: Bar Centro, Cin Cin Bar & Lounge
    - 15 room types: Virginia Petite/Deluxe, Skyline Superior/Deluxe, Sierra Luxury/Studio/Executive, Corner Spa, Spa One Bed, Player Spa Small/Large, VIP Hospitality, Virginia Twenty One
    - All with real WP content and hero images via Vercel CDN
    - Total venue pages: 55 (29 existing + 26 new)

--- src/data/venues/index.ts
+++ src/data/venues/index.ts
@@ -32,6 +32,35 @@ import carsonAmenitiesData from "./carson-valley-inn-amenities.json";
 import harveysPoolData from "./harveys-heated-pool.json";
 import harveysFitnessData from "./harveys-fitness-center.json";
 import playersCornerData from "./players-corner-spa-suite.json";
+// Eldorado Restaurants
+import eldoRoxyData from "./eldorado-roxy.json";
+import eldoLaStradaData from "./eldorado-la-strada.json";
+import eldoBrewBrothersData from "./eldorado-brew-brothers.json";
+import eldoMilliesData from "./eldorado-millies.json";
+import eldoIchibanData from "./eldorado-ichiban.json";
+import eldoSushiSakeData from "./eldorado-sushi-sake.json";
+import eldoHiddenPizzaData from "./eldorado-hidden-pizza.json";
+import eldoCoffeeCompanyData from "./eldorado-coffee-company.json";
+import eldoStarbucksData from "./eldorado-starbucks.json";
+// Eldorado Bars
+import eldoBarCentroData from "./eldorado-bar-centro.json";
+import eldoCinCinData from "./eldorado-cin-cin.json";
+// Eldorado Rooms
+import eldoVirginiaPetiteData from "./eldorado-virginia-petite-1-queen.json";
+import eldoVirginiaDeluxeData from "./eldorado-virginia-deluxe-2-queens.json";
+import eldoSkylineSuperior1KData from "./eldorado-skyline-superior-1-king.json";
+import eldoSkylineSuperior2QData from "./eldorado-skyline-superior-2-queens.json";
+import eldoSkylineDeluxeData from "./eldorado-skyline-deluxe-1-king.json";
+import eldoSierraLuxury1KData from "./eldorado-sierra-luxury-1-king.json";
+import eldoSierraLuxury2QData from "./eldorado-sierra-luxury-2-queen.json";
+import eldoSierraStudioData from "./eldorado-sierra-studio-suite.json";
+import eldoSierraExecData from "./eldorado-sierra-executive-suite.json";
+import eldoCornerSpaData from "./eldorado-corner-spa-suite.json";
+import eldoSpaOneBedData from "./eldorado-spa-one-bedroom-suite.json";
+import eldoPlayerSpaSmallData from "./eldorado-skyline-player-spa-small.json";
+import eldoPlayerSpaLargeData from "./eldorado-skyline-player-spa-large.json";
+import eldoVipHospitalityData from "./eldorado-skyline-vip-hospitality.json";
+import eldoVirginia21Data from "./eldorado-virginia-twenty-one-suite.json";
 
 export const ALL_VENUES = [
   chapelTavernData,
@@ -63,6 +92,35 @@ export const ALL_VENUES = [
   harveysPoolData,
   harveysFitnessData,
   playersCornerData,
+  // Eldorado Restaurants
+  eldoRoxyData,
+  eldoLaStradaData,
+  eldoBrewBrothersData,
+  eldoMilliesData,
+  eldoIchibanData,
+  eldoSushiSakeData,
+  eldoHiddenPizzaData,
+  eldoCoffeeCompanyData,
+  eldoStarbucksData,
+  // Eldorado Bars
+  eldoBarCentroData,
+  eldoCinCinData,
+  // Eldorado Rooms
+  eldoVirginiaPetiteData,
+  eldoVirginiaDeluxeData,
+  eldoSkylineSuperior1KData,
+  eldoSkylineSuperior2QData,
+  eldoSkylineDeluxeData,
+  eldoSierraLuxury1KData,
+  eldoSierraLuxury2QData,
+  eldoSierraStudioData,
+  eldoSierraExecData,
+  eldoCornerSpaData,
+  eldoSpaOneBedData,
+  eldoPlayerSpaSmallData,
+  eldoPlayerSpaLargeData,
+  eldoVipHospitalityData,
+  eldoVirginia21Data,
 ] as const;
 
 export type VenueData = (typeof ALL_VENUES)[number];
